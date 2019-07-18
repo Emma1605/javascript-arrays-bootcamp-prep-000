e72f1e1616719b071543497cb743c7234496b670
debonair-proc-5718 <jeanemma6@gmail.com>
2019-07-18 19:25:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element)
 { 
-  ["foo", ...]
+  return "foo", ...
 }
 function destructivelyAddElementToBeginningOfArray(array, element)
 {
